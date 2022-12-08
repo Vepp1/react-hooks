df67aaf870ec087baea034471e1508678921f859
Vepp1 <vinipontarolla@gmail.com>
2022-12-08 20:28:43 +0000

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UseStateWithObjects from './components/UseStateWithObjects';
+
+function App() {
+  return (
+    <div className="App">
+      <UseStateWithObjects />
+    </div>
+  );
+}
+
+export default App;
